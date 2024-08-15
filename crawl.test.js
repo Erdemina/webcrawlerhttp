4d8bea44c7f38eb9550ae90464a2f3d3066290a1
Erdemina <34918271+Erdemina@users.noreply.github.com>
2024-08-15 20:23:34 +0300

    normalizeURL function and tests for this function

---
+++ crawl.test.js
@@ -0,0 +1,31 @@
+const {normalizeURL} = require('./crawl.js')
+const {test, expect} = require ('@jest/globals')
+
+test('normalizeURL', ()=> {
+
+	const input = 'https://blog.boot.dev/path'
+	const actual = normalizeURL(input)
+	const expected = 'blog.boot.dev/path'
+	expect(actual).toEqual(expected)
+})
+test('normalizeURL strip trailing slash', () => {
+
+	const input = 'https://blog.boot.dev/path/'
+	const actual = normalizeURL(input)
+	const expected = 'blog.boot.dev/path'
+	expect(actual).toEqual(expected)
+})
+test('normalizeURL capitals', () => {
+
+	const input = 'HTTPS://BLOG.BoOt.Dev/path'
+	const actual = normalizeURL(input)
+	const expected = 'blog.boot.dev/path'
+	expect(actual).toEqual(expected)
+})
+test('normalizeURL strip http', () => {
+
+	const input = 'http://blog.boot.dev/path'
+	const actual = normalizeURL(input)
+	const expected = 'blog.boot.dev/path'
+	expect(actual).toEqual(expected)
+})
