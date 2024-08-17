05fa4614796e11f2779c1f25fbf07c61a921f9c2
Erdemina <34918271+Erdemina@users.noreply.github.com>
2024-08-17 13:26:48 +0300

    crawl page recursive

--- crawl.js
+++ crawl.js
@@ -1,27 +1,50 @@
 const{JSDOM} = require('jsdom')
 
 
-async function crawlPage(currentURL){
-	console.log(`actively crawling: ${currentURL}`)
+async function crawlPage(baseURL,currentURL,pages){
 
+	const baseURLObj = new URL(baseURL)
+	const currentURLObj = new URL(currentURL)
+	
+	if (baseURLObj.hostname != currentURLObj.hostname) {
+		return pages
+	}
+
+	const normalizedCurrentURL = normalizeURL(currentURL)
+	if (pages[normalizedCurrentURL] > 0){
+		pages[normalizedCurrentURL]++
+		return pages
+	}
+
+
+	pages[normalizedCurrentURL] = 1
+
+	console.log(`actively crawling: ${currentURL}`)
+	let htmlBody = ''
 	try {
 		const resp = await fetch(currentURL)
 		if (resp.status > 399) {
 			console.log(`error in fetch with status code: ${resp.status} on page: ${currentURL}`)
-			return
+			return pages
 		}
 
 		const contentType = resp.headers.get("content-type")
 		if (!contentType.includes("text/html")){
 			console.log(`non html response, content type: ${contentType}, on page: ${currentURL} `)
-			return
+			return pages
 		}
 
-		console.log(await resp.text())
+		htmlBody = await resp.text()
+
 	} catch (err) {
 		console.log(`error in fetch: ${err.message}, on page: ${currentURL}`)	
 	}
+	const nextURLs = getURLsFromHTML(htmlBody, baseURL)
 
+	for (const nextURL of nextURLs) {
+		pages = await crawlPage(baseURL, nextURL, pages)
+	}
+	return pages
 }
 
 
@@ -56,8 +79,6 @@ function getURLsFromHTML(htmlBody, baseURL) {
 function normalizeURL(urlString){
 	const urlObj = new URL(urlString)
 	const hostPath =  `${urlObj.hostname}${urlObj.pathname}`
-	console.log(hostPath.length)
-	console.log(hostPath.slice(-1))
 	if(hostPath.length > 0 && hostPath.slice(-1) == '/')
 	{
 		return hostPath.slice(0,-1)
